c3b1fb953ffabb5fdd9b7a6e633ee2acdb619a28
Petr Šulc <95595719+Petross808@users.noreply.github.com>
2024-02-01 14:36:01 +0000

    Solved question 2

--- src/main/java/CA3_Question2.java
+++ src/main/java/CA3_Question2.java
@@ -1,3 +1,6 @@
+import java.lang.reflect.Array;
+import java.util.HashSet;
+import java.util.Objects;
 import java.util.Scanner;
 import java.util.Stack;
 
@@ -21,7 +24,14 @@ public class CA3_Question2
                 arr[x][y] = 0;
             }
         }
-       return arr;
+
+        System.out.print("Row: ");
+        int r = kb.nextInt();
+        System.out.print("Column: ");
+        int c = kb.nextInt();
+        System.out.println();
+        fill(r, c, arr);
+        return arr;
     }
     /*
         Helper function to display the image
@@ -39,15 +49,70 @@ public class CA3_Question2
     }
     private static void fill(int r, int c, int[][] arr)
     {
+        Stack<Vector> toFill = new Stack();
+        Vector current;
+        int step = 0;
 
+        toFill.push(new Vector(c,r));
+        while(!toFill.empty())
+        {
+            step++;
+            current = toFill.pop();
+            arr[current.y][current.x] = step;
+            for(Vector next: current.GetNeighbours())
+            {
+                if(next.existsInTable(arr) && arr[next.y][next.x] == 0 && !toFill.contains(next))
+                {
+                    toFill.push(next);
+                }
+            }
+        }
+        display(arr);
     }
 
-    public static void start()
-    {
-       int[][] arr = floodFillStart();
-    }
+    public static void start() { int[][] arr = floodFillStart(); }
     public static void main(String[] args) {
         start();
+    }
+
+}
+
+class Vector
+{
+    public int x;
+    public int y;
+    Vector(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public Vector[] GetNeighbours()
+    {
+        return new Vector [] {
+                new Vector(x+1,y),
+                new Vector(x-1,y),
+                new Vector(x,y+1),
+                new Vector(x,y-1)
+        };
+    }
+
+    public boolean existsInTable(int[][] table)
+    {
+        return (this.x >= 0 && this.x < table[0].length && this.y >= 0 && this.y < table.length);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vector vector = (Vector) o;
+        return x == vector.x && y == vector.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 
 }
