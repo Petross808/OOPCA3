f3a81c0f10f1a5cdc8cbe21c42c0ec48ab221143
Petr Šulc <95595719+Petross808@users.noreply.github.com>
2024-02-08 14:49:21 +0000

    Solved Question 4

--- src/main/java/CA3_Question4.java
+++ src/main/java/CA3_Question4.java
@@ -3,8 +3,8 @@ import java.io.FileNotFoundException;
 import java.util.Scanner;
 import java.util.Stack;
 /**
- *  Name:
- *  Class Group:
+ *  Name: Petr Sulc
+ *  Class Group: GD2b
  */
 public class CA3_Question4 {
 
@@ -13,7 +13,25 @@ public class CA3_Question4 {
      */
     public static boolean validate(String filename) throws FileNotFoundException
     {
-        return false;
+        Scanner file = new Scanner(new File(filename));
+        Stack<String> tags = new Stack<>();
+        String next;
+        while(file.hasNext())
+        {
+            next = file.next();
+            if(next.matches("</?[A-Za-z]+>"))
+            {
+                if(next.startsWith("</") && !tags.empty() && tags.peek().equals(next.substring(2)))
+                {
+                    tags.pop();
+                }
+                else
+                {
+                    tags.push(next.substring(1));
+                }
+            }
+        }
+        return tags.empty();
     }
 
     /*
